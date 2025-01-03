33a71c5443047a086a51b275b33eefb85fb1fb80
aditya000099 <adi.agg099@gmail.com>
2025-01-03 17:27:03 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -80,7 +80,7 @@ export default function Home() {
     return (
         <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
             {/* Navbar */}
-            <nav className="bg-white p-4 text-zinc-900 flex justify-between items-center fixed top-0 z-50 w-screen">
+            <nav className="bg-transparent p-4 text-white flex justify-between items-center fixed top-0 z-50 w-screen">
                 <a href="/" className="text-xl font-bold">Holiday Planner</a>
                 <div className="space-x-6 mr-4">
                   <a href="#" className="hover:text-gray-300">Destinations</a>
@@ -98,7 +98,7 @@ export default function Home() {
                     <div ref={carouselRef} className="flex rounded transition-transform duration-500 ease-in-out" style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
                         {slides.map((slide, index) => (
                             <div key={index} className="w-full flex-shrink-0 relative">
-                                 <Image className="w-full h-full sm:h-[80vh] object-cover" src={slide.image} alt={slide.title} width={1200} height={600} />
+                                 <Image className="w-full h-full sm:h-[100vh] object-cover" src={slide.image} alt={slide.title} width={1200} height={600} />
                                  <div className="absolute bottom-0  text-white p-4 bg-gradient-to-t bg-black bg-opacity-70  w-full">
                                   <h2 className="text-2xl font-bold mb-2">{slide.title}</h2>
                                     <p className="mb-2">{slide.description}</p>
@@ -127,34 +127,34 @@ export default function Home() {
 
                 </section>
 
-                <section className="mb-8 p-6">
-                    <h2 className="text-3xl font-bold text-gray-800 mb-4">Popular Destinations</h2>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                        <div className="relative overflow-hidden rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
-                            <Image src="/bangkok.jpg" alt="Bangkok" width={400} height={300} className="object-cover w-full h-60" />
-                            <div className="p-4">
-                                <h3 className="text-xl font-semibold text-gray-800">Bangkok, Thailand</h3>
+                <section className="mb-8 p-6 sm:p-20">
+                    <h2 className="text-3xl font-extrabold text-gray-800 text-center mb-12">Popular Destinations</h2>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 px-20">
+                        <div className="relative overflow-hidden rounded-lg  transition-shadow duration-300">
+                            <Image src="/bangkok.jpg" alt="Bangkok" width={400} height={400} className="object-cover rounded-xl w-64 h-64" />
+                            <div className="py-4">
+                                <h3 className="text-xl font-extrabold text-gray-800">Bangkok, Thailand</h3>
                                 <p className="text-gray-600">From ₹799</p>
                             </div>
                         </div>
-                        <div className="relative overflow-hidden rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
-                            <Image src="/goa.jpg" alt="Goa" width={400} height={300} className="object-cover w-full h-60" />
-                            <div className="p-4">
-                                <h3 className="text-xl font-semibold text-gray-800">Goa, India</h3>
+                        <div className="relative overflow-hidden rounded-lg">
+                            <Image src="/goa.jpg" alt="Goa" width={400} height={400} className="object-cover rounded-xl w-64 h-64" />
+                            <div className="py-4">
+                                <h3 className="text-xl font-extrabold text-gray-800">Goa, India</h3>
                                 <p className="text-gray-600">From ₹899</p>
                             </div>
                         </div>
-                          <div className="relative overflow-hidden rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
-                            <Image src="/newyork.jpg" alt="New York" width={400} height={300} className="object-cover w-full h-60" />
-                            <div className="p-4">
-                                <h3 className="text-xl font-semibold text-gray-800">New York, USA</h3>
+                          <div className="relative overflow-hidden">
+                            <Image src="/newyork.jpg" alt="New York" width={400} height={400} className="object-cover rounded-xl w-64 h-64" />
+                            <div className="py-4">
+                                <h3 className="text-xl font-extrabold text-gray-800">New York, USA</h3>
                                 <p className="text-gray-600">From ₹999</p>
                             </div>
                         </div>
-                         <div className="relative overflow-hidden rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
-                            <Image src="/london.jpg" alt="London" width={400} height={300} className="object-cover w-full h-60" />
-                            <div className="p-4">
-                                <h3 className="text-xl font-semibold text-gray-800">London, UK</h3>
+                         <div className="relative overflow-hidden ">
+                            <Image src="/london.jpg" alt="London" width={400} height={400} className="object-cover rounded-xl w-64 h-64" />
+                            <div className="py-4">
+                                <h3 className="text-xl font-extrabold text-gray-800">London, UK</h3>
                                  <p className="text-gray-600">From ₹1199</p>
                             </div>
                         </div>
