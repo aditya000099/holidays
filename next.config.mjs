a92c392e5416ad0ca8f577c266cf4cd8dcccbd8a
aditya000099 <adi.agg099@gmail.com>
2025-02-02 03:08:37 +0530

    some jugaad for error

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        domains: ['cloud.appwrite.io', 'image1.com', 'image2.com'],
-    },
-    // async redirects() {
-    //     return [
-    //     {
-    //         source: "/countries",
-    //         destination: "/admin/countries",
-    //         permanent: true,
-    //     },
-    //     {
-    //         source: "/cities",
-    //         destination: "/admin/cities",
-    //         permanent: true,
-    //     },
-    //     {
-    //         source: "/packages",
-    //         destination: "/admin/packages",
-    //         permanent: true,
-    //     },
-    //     ];
-    // },
+  reactStrictMode: true,
+  images: {
+    domains: ["cloud.appwrite.io", "image1.com", "image2.com"],
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  // async redirects() {
+  //     return [
+  //     {
+  //         source: "/countries",
+  //         destination: "/admin/countries",
+  //         permanent: true,
+  //     },
+  //     {
+  //         source: "/cities",
+  //         destination: "/admin/cities",
+  //         permanent: true,
+  //     },
+  //     {
+  //         source: "/packages",
+  //         destination: "/admin/packages",
+  //         permanent: true,
+  //     },
+  //     ];
+  // },
 };
 
 export default nextConfig;
