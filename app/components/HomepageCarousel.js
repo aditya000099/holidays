893d50355fc2e41aad79f1d4db8ba5b96c33745c
aditya000099 <adi.agg099@gmail.com>
2025-02-14 13:25:39 +0530

    home page

--- app/components/HomepageCarousel.js
+++ app/components/HomepageCarousel.js
@@ -1,35 +1,22 @@
-'use client'
+"use client";
+
 import Image from "next/image";
+import { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
-import { useRef, useState } from "react";
-import { BsSearch } from "react-icons/bs";
-import { CiCalendar, CiLocationOn, CiUser } from "react-icons/ci";
 
 export default function HomepageCarousel() {
   const slides = [
     {
-      image: "/thailand.jpg",
-      title: "Explore Thailand's Wonders",
-      description:
-        "Discover ancient temples, vibrant markets, and stunning beaches. Experience the magic of Thailand.",
-      country: "Thailand",
-      price: "₹29999",
+      image: "/tm.jpg",
+      country: "thailand",
     },
     {
-      image: "/india.jpg",
-      title: "Journey Through Incredible India",
-      description:
-        "Immerse yourself in the rich culture, diverse landscapes, and spiritual heritage of India.",
-      country: "India",
-      price: "₹89999",
+      image: "/tungnath.jpg",
+      country: "india",
     },
     {
-      image: "/sydney.jpg",
-      title: "Visit Sydney",
-      description:
-        "Discover this iconic city, amazing landscapes, and beautiful beaches",
-      country: "Australia",
-      price: "₹129999",
+      image: "/mun.jpg",
+      country: "india",
     },
   ];
 
@@ -37,137 +24,117 @@ export default function HomepageCarousel() {
   const carouselRef = useRef(null);
   const router = useRouter();
 
-  const handlePrevSlide = () => {
-    setCurrentSlide((prevSlide) =>
-      prevSlide === 0 ? slides.length - 1 : prevSlide - 1
-    );
-  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+    }, 20000);
+    return () => clearInterval(interval);
+  }, [slides.length]);
 
-  const handleNextSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
-  };
-
-  const handleCarouselClick = (slide) => {
-    router.push(`/countries/${slide.country}`);
+  const handleCarouselClick = () => {
+    router.push(`/countries/India`);
   };
 
   return (
-    <>
-      <section className="relative overflow-hidden mb-12">
-        <div
-          ref={carouselRef}
-          className="flex rounded transition-transform duration-500 ease-in-out"
-          style={{ transform: `translateX(-${currentSlide * 100}%)` }}
-        >
-          {slides.map((slide, index) => (
-            <div
-              key={index}
-              className="w-full flex-shrink-0 relative cursor-pointer"
-              onClick={() => handleCarouselClick(slide)}
-            >
-              <Image
-                className="w-full h-full sm:h-[100vh] object-cover"
-                src={slide.image}
-                alt={slide.title}
-                width={1200}
-                height={600}
-              />
-              <div className="absolute top-20 sm:top-16 mx-2 rounded-xl  text-white p-4 w-fit">
-                <div className="bg-gradient-to-t from-white/20 rounded-full to-transparent bg-opacity-50 p-4">
-                  <h2 className="text-md sm:text-xl font-medium">
-                    {slide.title}
-                  </h2>
-                </div>
+    <section className="relative overflow-hidden mb-12">
+      <div
+        ref={carouselRef}
+        className="flex rounded transition-opacity duration-1000 ease-in-out"
+        style={{ transform: `translateX(-${currentSlide * 100}%)`, opacity: 1 }}
+      >
+        {slides.map((slide, index) => (
+          <div key={index} className="w-full flex-shrink-0 relative">
+            <div className="absolute inset-0 bg-black opacity-20"></div>
+            <Image
+              className="w-full h-full sm:h-[90vh] object-cover duration-1000 ease-in-out"
+              src={slide.image}
+              alt="India"
+              width={12000}
+              height={12000}
+              style={{ opacity: index === currentSlide ? 1 : 0 }}
+              onClick={() => handleCarouselClick()}
+            />
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white p-4 w-fit text-center">
+              <div className=" bg-opacity-50 rounded-md p-4">
+                <h2 className="text-xl sm:text-5xl  font-bold font-sans">
+                  Explore | Customize | Travel
+                </h2>
               </div>
             </div>
-          ))}
-        </div>
-        {/* Arrows for navigation */}
-        <div className="absolute top-1/2 transform -translate-y-1/2 left-4 sm:left-8 text-white flex items-center">
-          <button
-            onClick={handlePrevSlide}
-            className="bg-gray-700 hover:bg-gray-600 rounded-full p-2"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="h-6 w-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 19l-7-7 7-7"
-              />
-            </svg>
-          </button>
-        </div>
-
-        <div className="absolute top-1/2 transform -translate-y-1/2 right-4 sm:right-8 text-white flex items-center">
-          <button
-            onClick={handleNextSlide}
-            className="bg-gray-700 hover:bg-gray-600 rounded-full p-2"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="h-6 w-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 5l7 7-7 7"
-              />
-            </svg>
-          </button>
-        </div>
-        <div className="p-6 bg-gray-100 shadow-md relative z-20 rounded-xl mx-2 sm:mx-16 mt-2 sm:-mt-32 ">
-          <div className="bg-white  p-4 rounded-xl flex flex-col sm:flex-row justify-center gap-4 items-center">
-            <div className="flex items-center w-full sm:w-auto">
-              <CiLocationOn className="text-gray-700 text-xl mr-2" />
-              <input
-                type="text"
-                placeholder="Location"
-                className="bg-transparent border-none placeholder:text-gray-500 focus:ring-0 focus:outline-none w-full"
-              />
-            </div>
-            <div className="flex items-center w-full sm:w-auto">
-              <CiCalendar className="text-gray-700 text-xl mr-2" />
-              <input
-                type="text"
-                placeholder="Check In"
-                className="bg-transparent border-none placeholder:text-gray-500 focus:ring-0 focus:outline-none w-full"
-              />
-            </div>
-            <div className="flex items-center w-full sm:w-auto">
-              <CiCalendar className="text-gray-700 text-xl mr-2" />
-              <input
-                type="text"
-                placeholder="Check Out"
-                className="bg-transparent border-none placeholder:text-gray-500 focus:ring-0 focus:outline-none w-full"
-              />
-            </div>
-
-            <div className="flex items-center w-full sm:w-auto">
-              <CiUser className="text-gray-700 text-xl mr-2" />
-              <input
-                type="number"
-                placeholder="Guests"
-                className="bg-transparent border-none placeholder:text-gray-500 focus:ring-0 focus:outline-none w-full"
-              />
-            </div>
-
-            <button className="bg-indigo-500 text-white p-2 rounded-full text-center">
-              <BsSearch />
-            </button>
           </div>
-        </div>
-      </section>
-    </>
+        ))}
+      </div>
+    </section>
   );
 }
+
+// "use client";
+
+// import Image from "next/image";
+// import { useState, useEffect, useRef } from "react";
+// import { useRouter } from "next/navigation";
+
+// export default function HomepageCarousel() {
+//   const slides = [
+//     {
+//       image: "/tm.jpg",
+//       country: "thailand",
+//     },
+//     {
+//       image: "/tungnath.jpg",
+//       country: "india",
+//     },
+//     {
+//       image: "/mun.jpg",
+//       country: "australia",
+//     },
+//   ];
+
+//   const [currentSlide, setCurrentSlide] = useState(0);
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+//     }, 10000);
+//     return () => clearInterval(interval);
+//   }, [slides.length]);
+
+//   const handleCarouselClick = () => {
+//     router.push(`/countries/India`);
+//   };
+
+//   return (
+//     <section className="relative overflow-hidden mb-12">
+//       <div className="relative w-full h-full sm:h-[90vh] object-cover">
+//         {slides.map((slide, index) => (
+//           <div
+//             key={index}
+//             className="absolute inset-0 transition-opacity duration-1000 ease-in-out"
+//             style={{ opacity: index === currentSlide ? 1 : 0 }}
+//             onClick={() => handleCarouselClick()}
+//           >
+//             {/* Add 10% black film here with an empty div*/}
+//             <div className="absolute inset-0 bg-black opacity-10"></div>
+//             <Image
+//               className="w-full h-full sm:h-[90vh] object-cover"
+//               src={slide.image}
+//               alt="India"
+//               width={12000}
+//               height={12000}
+//               style={{ objectFit: "cover" }}
+//               onClick={() => handleCarouselClick()}
+//             />
+//             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white p-4 w-fit text-center">
+//               <div className=" bg-opacity-50 rounded-md p-4">
+//                 <h2 className="text-xl sm:text-5xl font-medium">
+//                   Explore | Customise | Travel
+//                 </h2>
+//               </div>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </section>
+//   );
+// }
--- app/loading.jsx
+++ app/loading.jsx
@@ -1,10 +1,14 @@
-'use client'
-const loading = () => {
-    return (
-        <div>
-        Loading
-        </div>
-    );
-}
+"use client";
 
-export default loading;
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white text-zinc-900 font-[family-name:var(--font-geist-sans)]">
+      <div className="flex flex-col items-center space-y-4">
+        <div className="animate-spin rounded-full h-12 w-12 border-4 border-zinc-300 border-t-transparent"></div>
+        <p className="text-lg font-medium">Loading...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
